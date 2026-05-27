d72f72386155c358a0101036836cc8099906c684
prashant <prashant15008@gmail.com>
2026-05-27 21:38:56 +0530

    fixed render deployment

--- alert_bot.py
+++ alert_bot.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
 BOT_TOKEN = "7961670645:AAFjvkLR3DELvlHlkEl-2Kc41CxXeUIefJ4"
 CHAT_ID = "242367281"
+
 
 def send_telegram_message(message):
 
@@ -17,16 +19,26 @@ def send_telegram_message(message):
 
     requests.post(url, data=data)
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
 
     data = request.json
 
-    message = data.get("message", "New Trading Alert")
+    message = data.get("message", "🚨 New Trading Alert")
 
     send_telegram_message(message)
 
-    return "Alert Sent"
+    return "Alert Sent Successfully"
+
+
+@app.route('/')
+def home():
+    return "Telegram Trading Alert Bot is Running 🚀"
+
 
 if __name__ == "__main__":
-    app.run(port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
